d8440bfac0974085ed69f7198381ecdefb73d790
mracoon <nataliyakomarova777@gmail.com>
2022-08-10 11:45:54 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minVal = min
+        this.maxVal = max
     }
 
     guess() {
+        this.candidate = Math.round((this.maxVal - this.minVal) / 2 + this.minVal)
 
+        return this.candidate
     }
 
     lower() {
 
+        this.maxVal = this.candidate
     }
 
     greater() {
-
+        this.minVal = this.candidate
     }
 }
 
